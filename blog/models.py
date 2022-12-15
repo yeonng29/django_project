a410c693aea51c3fd07c824d5dabea939ae5b78e
yeonng29 <yeonng29@naver.com>
2022-12-15 12:02:48 +0900

    author 필드 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,25 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
+
+
+# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
-    file_uploda = models.FileField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # author:추후 작성 예정
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
 
     def get_file_name(self):
-        return os.path.basename(self.file_uploda.name)
+        return os.path.basename(self.file_upload.name)
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
