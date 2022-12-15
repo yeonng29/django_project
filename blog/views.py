24fcc2755d477d36c9ba29554c3ef6a17a9584ac
yeonng29 <yeonng29@naver.com>
2022-12-15 14:49:15 +0900

    카테고리 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post, Category
 
@@ -18,18 +18,6 @@ class PostDetail(DetailView):
     model = Post
 
 
-# Create your views here.
-# def index(request):
-#  posts = Post.objects.all().order_by('-pk')
-#
-#   return render(
-#      request,
-#     'blog/index.html',
-#      {
-#          'posts' : posts,
-#    }
-#   )
-
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
@@ -38,5 +26,24 @@ def single_post_page(request, pk):
         'blog/single_post_page.html',
         {
             'post': post,
+        }
+    )
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category ='미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'categories' : Category.objects.all(),
+            'no_category_post_count' : Post.objects.filter(category=None).count(),
+            'category' : category,
         }
     )
